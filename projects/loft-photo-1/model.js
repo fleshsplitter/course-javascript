0de35bf949a1c6ed450c2b36b2e37ad8f26aec19
Alex <horrorinls@mail.ru>
2023-03-12 14:30:26 +0600

    finish 2nd week

--- projects/loft-photo-1/model.js
+++ projects/loft-photo-1/model.js
@@ -4,6 +4,18 @@ import photosDB from './photos.json';
 import friendsDB from './friends.json';
 
 export default {
-  getRandomElement(array) {},
-  getNextPhoto() {},
+  getRandomElement(array) {
+    if (!array.length) {
+      return null;
+    }
+  
+    return arr[parseInt(Math.random() * (arr.length - 1))];
+  },
+  getNextPhoto() {
+    const friend = this.getRandomElement(friendsDB);
+    const photos = photosDB[friend.id];
+    const photo = this.getRandomElement(photos);
+
+    return {friend, url: photo.url};
+  },
 };
